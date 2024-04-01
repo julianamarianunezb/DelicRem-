00a4e4db8b2994ec449fecd88d28fd3deb1d87a0
julianamarianunezb <juliananarchivos@gmail.com>
2024-03-31 20:34:20 -0500

    Frontend delicrem flutter

---
+++ frontend/lib/screens/login_screen.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'menu_principal_screen.dart'; 
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String _errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'DelicRem+',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255, 236, 213, 231), Color.fromARGB(255, 169, 220, 221)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Icon(
+                Icons.person_outline_rounded,
+                size: 150,
+                color: Color.fromARGB(255, 181, 11, 144),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextField(
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Nombre de Usuario',
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                      ),
+                      style: const TextStyle(color: Colors.black),
+                      cursorColor: Colors.black,
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Contraseña',
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                      ),
+                      obscureText: true,
+                      style: const TextStyle(color: Colors.black),
+                      cursorColor: Colors.black,
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: _login,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 232, 145, 226),
+                      ),
+                      child: const Text(
+                        'Iniciar Sesión',
+                        style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                    ),
+                    if (_errorMessage.isNotEmpty)
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            _errorMessage,
+                            style: const TextStyle(color: Color.fromARGB(255, 133, 16, 8)),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20), 
+              const Align(
+                alignment: Alignment.bottomRight, 
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Por Juliana Nuñez',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 89, 88, 88), 
+                      fontStyle: FontStyle.italic, 
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _login() {
+    final username = _usernameController.text;
+    final password = _passwordController.text;
+
+    if (username == 'admin' && password == '1234') {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MenuPrincipalScreen()),
+      );
+    } else {
+      setState(() {
+        _errorMessage = 'Nombre de usuario o contraseña incorrectos';
+      });
+    }
+  }
+}
